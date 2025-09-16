1390fc138b6ccdc23592ef147bca5995cb9d3986
Sachin Dilshan <sachin.d@wemakeplatforms.io>
2025-09-17 00:06:31 +0530

    added dark/light theme modes

--- src/app/components/shared/header/header.ts
+++ src/app/components/shared/header/header.ts
@@ -1,14 +1,19 @@
-import { ViewportScroller } from '@angular/common';
+import { NgOptimizedImage, ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import {ThemeService} from '../../../core/theme.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-  constructor(private readonly viewportScroller: ViewportScroller) {}
+  constructor(
+    private readonly viewportScroller: ViewportScroller,
+    public readonly themeService: ThemeService,
+  ) {}
 
   scrollToSection(sectionId: string) {
     this.viewportScroller.scrollToAnchor(sectionId);
@@ -20,5 +25,13 @@ export class Header {
       '_blank',
       'noopener,noreferrer'
     );
+  }
+
+  toggleTheme() {
+    this.themeService.toggle();
+  }
+
+  useSystemTheme() {
+    this.themeService.useSystem();
   }
 }
